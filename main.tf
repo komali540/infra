d7354d95ec6406bad02b2790afe49ace8cd2f8d4
komali540 <yerriboinakomali12@gmail.com>
2025-03-15 14:12:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
 provider "aws" {
 region = "us-east-1"
 }
-resource "aws_instance" "three" {
-  subnet_id     = aws_subnet.two.id
-  ami           = "ami-0866a3c8686eaeeba"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "dev-server"
-  }
+
+resource "aws_instance" "one" {
+ami = "ami-03eb6185d756497f8"
+instance_type = "t2.micro"
 }
